31dd21f3d2289faec9ebb6b60fa4967337a3e505
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-24 19:26:32 +0300

    Add simple test to extracting requires

--- test/html-require-postloader.test.js
+++ test/html-require-postloader.test.js
@@ -6,5 +6,12 @@ describe('html-require-postloader', function () {
 
     it('should exist', function () {
         expect(loader).to.be.defined;
-    })
+    });
+
+    it('should parse source and extract pieces as requires', function () {
+        var source = 'module.exports = "test foo marker foo value=\'requiredValue\' foo"';
+        var result = loader(source);
+
+        expect(result).to.contain('require(\'requiredValue\')');
+    });
 });
