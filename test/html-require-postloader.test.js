6312bb3ba8e516021b35ff2b38871c173c98424f
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-24 19:22:31 +0300

    Set up test infrastructure

---
+++ test/html-require-postloader.test.js
@@ -0,0 +1,10 @@
+
+var expect = require('chai').expect;
+var loader = require('../lib/html-require-postloader');
+
+describe('html-require-postloader', function () {
+
+    it('should exist', function () {
+        expect(loader).to.be.defined;
+    })
+});
