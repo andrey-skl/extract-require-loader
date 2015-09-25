d1dbfa1de9e1f6b067edfae966a41bb3657b6510
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-25 13:59:03 +0300

    Extracting requires without replacing html content

--- test/html-require-postloader.test.js
+++ test/html-require-postloader.test.js
@@ -1,17 +1,28 @@
-
 var expect = require('chai').expect;
 var loader = require('../lib/html-require-postloader');
 
 describe('html-require-postloader', function () {
+    var defaultQuery = {
+        requireTemplate: 'require("icon/icon/source/#value#.svg");',
+        markers:[
+            {
+                attribute: 'marker',
+                valueRegExp: 'value=.([a-z]*)',
+                stopWord: '>'
+            }
+        ]
+    };
 
     it('should exist', function () {
         expect(loader).to.be.defined;
     });
 
     it('should parse source and extract pieces as requires', function () {
-        var source = 'module.exports = "test foo marker foo value=\'requiredValue\' foo"';
-        var result = loader(source);
+        var query = '?' + JSON.stringify(defaultQuery);
+        var source = 'module.exports = "test foo marker foo value=\'requiredValue\' foo test > bar"';
 
-        expect(result).to.contain('require(\'requiredValue\')');
+        var result = loader.call({query: query}, source);
+
+        expect(result).to.contain('require("icon/icon/source/requiredValue.svg")');
     });
 });
